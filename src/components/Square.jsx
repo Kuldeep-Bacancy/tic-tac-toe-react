e22fa87e120d61f2c5f665c2ca5151ae2987ce3d
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-01-02 11:05:50 +0530

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Square({ value, handleSquareClick }) {
+  return (
+    <button className="square" onClick={handleSquareClick}>{value}</button>
+  )
+}
+
+export default Square
